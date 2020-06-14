0c2f8894e9dcf79ac70295121cb9b6bfbf58d1f8
Сергей Грищенков <grishenkov.sergei@gmail.com>
2020-06-13 22:23:13 -0500

    Готова первая часть первого задания

--- js/main.js
+++ js/main.js
@@ -1,48 +1,78 @@
 'use strict';
-//Константы
-var ARRAYOPTIONS = [];
 
-// Функция создания массива
-var createArrayOptions = function () {
-  for (var i = 0; i < 8; i++) {
+var TYPES = ['palace', 'flat', 'house', 'bungalo'];
+var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var TIMES = ['12:00', '13:00', '14:00'];
 
-    var meta = {
-      'author': {
-        'avatar': ('img / avatars / user' + 0 + ' + [i].png')
-      },
-      'offer': {
-        'title': title,
-        'address': (locationX, locationY),
-        'price': (number, price),
-        'type': (palace, flat, house, bungalo),
-        'rooms': rooms,
-        'guests': guests,
-        'checkin': ('12:00, 13: 00, 14: 00'),
-        'checkout': ('12:00, 13:00, 14:00'),
-        'features': ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
-        'description': 'description',
-        'photos': {
-            ('http://o0.github.io/assets/images/tokyo/hotel1.jpg'),
-            ('http://o0.github.io/assets/images/tokyo/hotel2.jpg'),
-            ('http://o0.github.io/assets/images/tokyo/hotel3.jpg'),
-  }
-}
-location = {
-  'x': //случайное число, координата x метки на карте. Значение ограничено размерами блока, в котором перетаскивается метка,
-    'y': //случайное число, координата y метки на карте от 130 до 630,
-}
-  }
-    };
-ARRAYOPTIONS.push(meta[i]);
-  };
-
-// Включение карты
 var map = document.querySelector('.map');
+var mapPins = document.querySelector('.map__pins');
+var pinElement = document.querySelector('#pin').content.querySelector('.map__pin');
+var pinHeight = 50;
+var pinWidth = 70;
+
+// случайное число от min до (max+1)
+function randomInteger(min, max) {
+  var rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+}
+
+//Случайная длина массива
+var randomArray = function (array) {
+  return array.slice(0, randomInteger(1, array.length - 1));
+};
+
+// Функция добавления объекта
+var createAd = function (n) {
+  var rect = map.getBoundingClientRect();
+  var location = {
+    x: randomInteger(rect.left - pinHeight, rect.width - pinWidth/2),
+    y: randomInteger(130, 630),
+  };
+  return {
+    author: {
+      avatar: 'img/avatars/user0' + n + '.png',
+    },
+    offer: {
+      title: 'Some title',
+      adress: location.x + ', ' + location.y,
+      price: 'price',
+      type: TYPES[randomInteger(TYPES[0], TYPES.length - 1)],
+      rooms: 'rooms',
+      guests: 'guests',
+      checkin: TIMES[randomInteger(TIMES[0], TIMES.length - 1)],
+      checkout: TIMES[randomInteger(TIMES[0], TIMES.length - 1)],
+      features: randomArray(FEATURES),
+      description: 'description',
+      photos: randomArray(PHOTOS),
+    },
+    location: location
+  }
+}
+
+var createAds = function () {
+  var ads = [];
+  for (var i = 1; i <= 8; i++) {
+    ads.push(createAd(i));
+  }
+  return ads
+};
+var ads = createAds();
+
+var renderPins = function () {
+  var fragment = new DocumentFragment();
+
+  for (var i = 0; i < ads.length; i++) {
+    var clonePin = pinElement.cloneNode(true);
+    clonePin.style.left = ads[i].location.x + 'px';
+    clonePin.style.top = ads[i].location.y + 'px';
+    clonePin.querySelector('img').src = ads[i].author.avatar;
+    clonePin.querySelector('img').alt = ads[i].author.title;
+    fragment.appendChild(clonePin);
+  }
+  mapPins.appendChild(fragment);
+};
+
+renderPins();
+
 map.classList.remove('map--faded');
-
-
-// Функция случайных чисел
-var randomNumbers = function () { };
-
-// Функция наполнения DOM элементов
-var renderElements = function () { };
