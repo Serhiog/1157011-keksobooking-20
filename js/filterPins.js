1ec64c81954b24afb765932b893cd71a5d55b2e6
Сергей Грищенков <grishenkov.sergei@gmail.com>
2020-07-20 00:01:34 -0500

    Добавил третий фильтр по гостям

--- js/filterPins.js
+++ js/filterPins.js
@@ -5,6 +5,7 @@
   var housingType = document.querySelector('#housing-type')
   var housingPrice = document.querySelector('#housing-price')
   var housingRooms = document.querySelector('#housing-rooms')
+  var housingGuests = document.querySelector('#housing-guests')
   var unFiltredPins = [];
   var change = false
   var filtredAds = []
@@ -13,6 +14,7 @@
   var filtredByTypePins = [];
   var filtredByPricePins = []
   var filtredByRoomsPins = []
+  var filtredByGuestsPins = []
 
   var pinsFeatures = {
     maxPrice: '50000',
@@ -23,9 +25,11 @@
     anySelect: 'any',
     maxRooms: '3',
     midRooms: '2',
-    minRooms: '1'
+    minRooms: '1',
+    maxGuests: '2',
+    midGuests: '1',
+    minGuests: '0'
   }
-
 
   window.filterPins = {
 
@@ -46,6 +50,9 @@
     savedFiltredByRooms: function (filtredByTypePins) {
       filtredByRoomsPins = filtredByTypePins;
     },
+    savedFiltredByGuests: function (filtredByTypePins) {
+      filtredByGuestsPins = filtredByTypePins;
+    },
 
     changeType: function (evt) {
 
@@ -59,142 +66,173 @@
         window.renderPins.onSucces(unFiltredPins)
       }
 
-      // if (evt.target.value === 'palace') {
-      //   checkEmptyType = true
-      // }
+      if (evt.target.value === 'palace') {
+        checkEmptyType = true
+      }
 
       if (card) { card.remove() }
 
-      console.log(filtredByTypePins)
-      console.log(filtredByPricePins)
-      console.log(filtredByRoomsPins)
     },
 
     changePrice: function (evt) {
       housingRooms.value = pinsFeatures.anySelect
 
-      // if (checkEmptyType && filtredByTypePins.length === 0) {
-      //   window.renderPins.onSucces(emptyArray)
-      // }
-      // if (!checkEmptyType && filtredByTypePins.length === 0) {
-      //   filtredByTypePins = unFiltredPins
-      // }
-      if (evt.target.value === pinsFeatures.priceMaxLevel) {
-        if (filtredByTypePins.length === 0) {
-          filtredByPricePins = unFiltredPins.filter(item => item.offer.price >= pinsFeatures.maxPrice)
-          window.filterPins.savedFiltredByPrice(filtredByPricePins)
-          window.renderPins.onSucces(filtredByPricePins)
-        } else {
-          filtredByPricePins = filtredByTypePins.filter(item => item.offer.price >= pinsFeatures.maxPrice)
-          window.filterPins.savedFiltredByPrice(filtredByPricePins)
-          window.renderPins.onSucces(filtredByPricePins)
-        }
-        console.log(filtredByTypePins)
-        console.log(filtredByPricePins)
-        console.log(filtredByRoomsPins)
-      }
-      if (evt.target.value === pinsFeatures.priceLowLevel) {
-        if (filtredByTypePins.length === 0) {
-          filtredByPricePins = unFiltredPins.filter(item => item.offer.price < pinsFeatures.minPrice)
-          window.filterPins.savedFiltredByPrice(filtredByPricePins)
-          window.renderPins.onSucces(filtredByPricePins)
-        }
-        else {
-          filtredByPricePins = filtredByTypePins.filter(item => item.offer.price < pinsFeatures.minPrice)
-          window.filterPins.savedFiltredByPrice(filtredByPricePins)
-          window.renderPins.onSucces(filtredByPricePins)
-        }
-        console.log(filtredByTypePins)
-        console.log(filtredByPricePins)
-        console.log(filtredByRoomsPins)
-      }
-      if (evt.target.value === pinsFeatures.priceMidLevel) {
-        if (filtredByTypePins.length === 0) {
-          filtredByPricePins = unFiltredPins.filter(item => item.offer.price >= pinsFeatures.minPrice && item.offer.price < pinsFeatures.maxPrice)
-          window.filterPins.savedFiltredByPrice(filtredByPricePins)
-          window.renderPins.onSucces(filtredByPricePins)
-        } else {
-          filtredByPricePins = filtredByTypePins.filter(item => item.offer.price >= pinsFeatures.minPrice && item.offer.price < pinsFeatures.maxPrice)
-          window.filterPins.savedFiltredByPrice(filtredByPricePins)
-          window.renderPins.onSucces(filtredByPricePins)
-        }
-        console.log(filtredByTypePins)
-        console.log(filtredByPricePins)
-        console.log(filtredByRoomsPins)
-      }
+      if (checkEmptyType && filtredByTypePins.length === 0) {
+        window.renderPins.onSucces(emptyArray)
 
-      // if (evt.target.value === pinsFeatures.anySelect) {
-      //   window.renderPins.onSucces(filtredByTypePins)
-      // }
+      }
+      else {
+
+        if (evt.target.value === pinsFeatures.priceMaxLevel) {
+          if (filtredByTypePins.length === 0) {
+            filtredByPricePins = unFiltredPins.filter(item => item.offer.price >= pinsFeatures.maxPrice)
+            window.filterPins.savedFiltredByPrice(filtredByPricePins)
+            window.renderPins.onSucces(filtredByPricePins)
+          } else {
+            filtredByPricePins = filtredByTypePins.filter(item => item.offer.price >= pinsFeatures.maxPrice)
+            window.filterPins.savedFiltredByPrice(filtredByPricePins)
+            window.renderPins.onSucces(filtredByPricePins)
+          }
+        }
+        if (evt.target.value === pinsFeatures.priceLowLevel) {
+          if (filtredByTypePins.length === 0) {
+            filtredByPricePins = unFiltredPins.filter(item => item.offer.price < pinsFeatures.minPrice)
+            window.filterPins.savedFiltredByPrice(filtredByPricePins)
+            window.renderPins.onSucces(filtredByPricePins)
+          }
+          else {
+            filtredByPricePins = filtredByTypePins.filter(item => item.offer.price < pinsFeatures.minPrice)
+            window.filterPins.savedFiltredByPrice(filtredByPricePins)
+            window.renderPins.onSucces(filtredByPricePins)
+          }
+        }
+        if (evt.target.value === pinsFeatures.priceMidLevel) {
+          if (filtredByTypePins.length === 0) {
+            filtredByPricePins = unFiltredPins.filter(item => item.offer.price >= pinsFeatures.minPrice && item.offer.price < pinsFeatures.maxPrice)
+            window.filterPins.savedFiltredByPrice(filtredByPricePins)
+            window.renderPins.onSucces(filtredByPricePins)
+          } else {
+            filtredByPricePins = filtredByTypePins.filter(item => item.offer.price >= pinsFeatures.minPrice && item.offer.price < pinsFeatures.maxPrice)
+            window.filterPins.savedFiltredByPrice(filtredByPricePins)
+            window.renderPins.onSucces(filtredByPricePins)
+          }
+        }
+      }
 
       if (card) { card.remove() }
     },
 
     changeRooms: function (evt) {
 
-      // if (checkEmptyType && filtredByTypePins.length === 0) {
-      //   window.renderPins.onSucces(emptyArray)
-      // }
-      // if (!checkEmptyType && filtredByTypePins.length === 0) {
-      //   filtredByTypePins = unFiltredPins
-      // }
-      if (evt.target.value === pinsFeatures.maxRooms) {
-        if (filtredByTypePins.length === 0 && filtredByPricePins.length === 0) {
-          filtredByRoomsPins = unFiltredPins.filter(item => item.offer.rooms === parseInt(evt.target.value))
-          window.filterPins.savedFiltredByRooms(filtredByRoomsPins)
-          window.renderPins.onSucces(filtredByRoomsPins)
-        } else {
-          filtredByRoomsPins = filtredByPricePins.filter(item => item.offer.rooms === parseInt(evt.target.value))
-          window.filterPins.savedFiltredByRooms(filtredByRoomsPins)
-          window.renderPins.onSucces(filtredByRoomsPins)
-        }
-        console.log(filtredByTypePins)
-        console.log(filtredByPricePins)
-        console.log(filtredByRoomsPins)
-      }
-      if (evt.target.value === pinsFeatures.midRooms) {
-        if (filtredByTypePins.length === 0 && filtredByPricePins.length === 0) {
-          filtredByRoomsPins = unFiltredPins.filter(item => item.offer.rooms === parseInt(evt.target.value))
-          window.filterPins.savedFiltredByRooms(filtredByRoomsPins)
-          window.renderPins.onSucces(filtredByRoomsPins)
-        } else {
-          filtredByRoomsPins = filtredByPricePins.filter(item => item.offer.rooms === parseInt(evt.target.value))
-          window.filterPins.savedFiltredByRooms(filtredByRoomsPins)
-          window.renderPins.onSucces(filtredByRoomsPins)
-        }
-        console.log(filtredByTypePins)
-        console.log(filtredByPricePins)
-        console.log(filtredByRoomsPins)
-      }
-      if (evt.target.value === pinsFeatures.minRooms) {
-        if (filtredByTypePins.length === 0 && filtredByPricePins.length === 0) {
-          filtredByRoomsPins = unFiltredPins.filter(item => item.offer.rooms === parseInt(evt.target.value))
-          window.filterPins.savedFiltredByRooms(filtredByRoomsPins)
-          window.renderPins.onSucces(filtredByRoomsPins)
-        } else {
-          filtredByRoomsPins = filtredByPricePins.filter(item => item.offer.rooms === parseInt(evt.target.value))
-          window.filterPins.savedFiltredByRooms(filtredByRoomsPins)
-          window.renderPins.onSucces(filtredByRoomsPins)
-        }
-        console.log(filtredByTypePins)
-        console.log(filtredByPricePins)
-        console.log(filtredByRoomsPins)
-      }
-      // if (evt.target.value === pinsFeatures.anySelect) {
-      //   window.renderPins.onSucces(filtredByTypePins.filter(item => item.offer.rooms > pinsFeatures.maxRooms))
-      // }
-      // if (evt.target.value === pinsFeatures.priceLowLevel) {
-      //   window.renderPins.onSucces(filtredByTypePins.filter(item => item.offer.price < pinsFeatures.minPrice))
-      // }
-      // if (evt.target.value === pinsFeatures.priceMidLevel) {
-      //   window.renderPins.onSucces(filtredByTypePins.filter(item => item.offer.price >= pinsFeatures.minPrice && item.offer.price < pinsFeatures.maxPrice))
-      // }
+      housingGuests.value = pinsFeatures.anySelect
 
-      // if (evt.target.value === pinsFeatures.priceAnyLevel) {
-      //   window.renderPins.onSucces(filtredByTypePins)
-      // }
+      if (checkEmptyType && filtredByTypePins.length === 0) {
+        window.renderPins.onSucces(emptyArray)
 
-      if (card) { card.remove() }
+      }
+      else {
+        if (evt.target.value === pinsFeatures.maxRooms) {
+          if (filtredByTypePins.length === 0 && filtredByPricePins.length === 0) {
+            filtredByRoomsPins = unFiltredPins.filter(item => item.offer.rooms === parseInt(evt.target.value))
+            window.filterPins.savedFiltredByRooms(filtredByRoomsPins)
+            window.renderPins.onSucces(filtredByRoomsPins)
+          } else {
+            filtredByRoomsPins = filtredByPricePins.filter(item => item.offer.rooms === parseInt(evt.target.value))
+            window.filterPins.savedFiltredByRooms(filtredByRoomsPins)
+            window.renderPins.onSucces(filtredByRoomsPins)
+          }
+        }
+        if (evt.target.value === pinsFeatures.midRooms) {
+          if (filtredByTypePins.length === 0 && filtredByPricePins.length === 0) {
+            filtredByRoomsPins = unFiltredPins.filter(item => item.offer.rooms === parseInt(evt.target.value))
+            window.filterPins.savedFiltredByRooms(filtredByRoomsPins)
+            window.renderPins.onSucces(filtredByRoomsPins)
+          } else {
+            filtredByRoomsPins = filtredByPricePins.filter(item => item.offer.rooms === parseInt(evt.target.value))
+            window.filterPins.savedFiltredByRooms(filtredByRoomsPins)
+            window.renderPins.onSucces(filtredByRoomsPins)
+          }
+        }
+        if (evt.target.value === pinsFeatures.minRooms) {
+          if (filtredByTypePins.length === 0 && filtredByPricePins.length === 0) {
+            filtredByRoomsPins = unFiltredPins.filter(item => item.offer.rooms === parseInt(evt.target.value))
+            window.filterPins.savedFiltredByRooms(filtredByRoomsPins)
+            window.renderPins.onSucces(filtredByRoomsPins)
+          } else {
+            filtredByRoomsPins = filtredByPricePins.filter(item => item.offer.rooms === parseInt(evt.target.value))
+            window.filterPins.savedFiltredByRooms(filtredByRoomsPins)
+            window.renderPins.onSucces(filtredByRoomsPins)
+          }
+        }
+        if (evt.target.value === pinsFeatures.anySelect) {
+          if (filtredByTypePins.length === 0 && filtredByPricePins.length === 0) {
+            filtredByRoomsPins = unFiltredPins
+            window.filterPins.savedFiltredByRooms(filtredByRoomsPins)
+            window.renderPins.onSucces(filtredByRoomsPins)
+          } else {
+            filtredByRoomsPins = unFiltredPins
+            window.filterPins.savedFiltredByRooms(filtredByRoomsPins)
+            window.renderPins.onSucces(filtredByRoomsPins)
+          }
+        }
+
+        if (card) { card.remove() }
+      }
+    },
+    changeGuests: function (evt) {
+
+      if (checkEmptyType && filtredByTypePins.length === 0) {
+        window.renderPins.onSucces(emptyArray)
+      }
+      else {
+        if (evt.target.value === pinsFeatures.maxGuests) {
+          if (filtredByTypePins.length === 0 && filtredByPricePins.length === 0 && filtredByRoomsPins.length === 0) {
+            filtredByGuestsPins = unFiltredPins.filter(item => item.offer.guests === parseInt(evt.target.value))
+            window.filterPins.savedFiltredByGuests(filtredByGuestsPins)
+            window.renderPins.onSucces(filtredByGuestsPins)
+          } else {
+            filtredByGuestsPins = filtredByRoomsPins.filter(item => item.offer.guests === parseInt(evt.target.value))
+            window.filterPins.savedFiltredByGuests(filtredByGuestsPins)
+            window.renderPins.onSucces(filtredByGuestsPins)
+          }
+        }
+        if (evt.target.value === pinsFeatures.midGuests) {
+          if (filtredByTypePins.length === 0 && filtredByPricePins.length === 0 && filtredByRoomsPins.length === 0) {
+            filtredByGuestsPins = unFiltredPins.filter(item => item.offer.guests === parseInt(evt.target.value))
+            window.filterPins.savedFiltredByGuests(filtredByGuestsPins)
+            window.renderPins.onSucces(filtredByGuestsPins)
+          } else {
+            filtredByGuestsPins = filtredByRoomsPins.filter(item => item.offer.guests === parseInt(evt.target.value))
+            window.filterPins.savedFiltredByGuests(filtredByGuestsPins)
+            window.renderPins.onSucces(filtredByGuestsPins)
+          }
+        }
+        if (evt.target.value === pinsFeatures.minGuests) {
+          if (filtredByTypePins.length === 0 && filtredByPricePins.length === 0 && filtredByRoomsPins.length === 0) {
+            filtredByGuestsPins = unFiltredPins.filter(item => item.offer.guests < pinsFeatures.midGuests)
+            window.filterPins.savedFiltredByGuests(filtredByGuestsPins)
+            window.renderPins.onSucces(filtredByGuestsPins)
+          } else {
+            filtredByGuestsPins = filtredByRoomsPins.filter(item => item.offer.guests < pinsFeatures.midGuests)
+            window.filterPins.savedFiltredByGuests(filtredByGuestsPins)
+            window.renderPins.onSucces(filtredByGuestsPins)
+          }
+
+        }
+        if (evt.target.value === pinsFeatures.anySelect) {
+          if (filtredByTypePins.length === 0 && filtredByPricePins.length === 0 && filtredByRoomsPins.length === 0) {
+            filtredByGuestsPins = unFiltredPins
+            window.filterPins.savedFiltredByGuests(filtredByGuestsPins)
+            window.renderPins.onSucces(filtredByGuestsPins)
+          } else {
+            filtredByGuestsPins = filtredByRoomsPins.filter(item => item.offer.guests > pinsFeatures.maxGuests)
+            window.filterPins.savedFiltredByGuests(filtredByGuestsPins)
+            window.renderPins.onSucces(filtredByGuestsPins)
+          }
+        }
+
+        if (card) { card.remove() }
+      }
     }
   }
 
@@ -203,6 +241,7 @@
   housingType.addEventListener('change', window.filterPins.changeType)
   housingPrice.addEventListener('change', window.filterPins.changePrice)
   housingRooms.addEventListener('change', window.filterPins.changeRooms)
+  housingGuests.addEventListener('change', window.filterPins.changeGuests)
 
 
 })();
