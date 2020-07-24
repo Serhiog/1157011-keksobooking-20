d41a1066a7f45d78d2cdf507dcf91c54c230a359
Сергей Грищенков <grishenkov.sergei@gmail.com>
2020-07-24 00:05:35 -0500

    добавил выбор фото жилья

--- js/avatar.js
+++ js/avatar.js
@@ -7,6 +7,9 @@
   var avatarChooser = document.querySelector('.ad-form-header__input');
   var userPicAvatar = document.querySelector('.ad-form-header__avatar');
 
+  var adPhotoChooser = document.querySelector('.ad-form__input');
+  var adPhoto = document.querySelector('.ad-form__photo');
+
   avatarChooser.addEventListener('change', function () {
     var file = avatarChooser.files[0];
     var fileName = file.name.toLowerCase();
@@ -22,6 +25,27 @@
         userPicAvatar.src = reader.result;
       });
       reader.readAsDataURL(file);
+    }
+  });
+
+  adPhotoChooser.addEventListener('change', function () {
+    var adFile = adPhotoChooser.files[0];
+    var adFileName = adFile.name.toLowerCase();
+
+    var adMatches = ALLOWED_TYPES.some(function (item) {
+      return adFileName.endsWith(item);
+    });
+
+    if (adMatches) {
+
+      var reader = new FileReader();
+      reader.addEventListener('load', function () {
+        var newPhoto = document.createElement('img');
+        newPhoto.style = 'width: inherit; height: inherit;';
+        newPhoto.src = reader.result;
+        adPhoto.appendChild(newPhoto);
+      });
+      reader.readAsDataURL(adFile);
     }
   });
 })();
